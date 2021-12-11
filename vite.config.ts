64be10590a399ebd2233e983cba99a2563777d0e
Robin Millette <robin@millette.info>
2021-12-11 16:29:00 -0500

    Add tailwind unocss preset.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import Unocss from 'unocss/vite'
+import presetWind from '@unocss/preset-wind'
 
 export default defineConfig({
-  plugins: [solidPlugin(), Unocss()],
+  plugins: [solidPlugin(), Unocss(
+    {
+      presets: [
+        presetWind()
+      ]
+    }    
+  )],
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
