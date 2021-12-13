81eaf075fd369d0c9daed256ef41a7a335f9be59
Robin Millette <robin@millette.info>
2021-12-13 10:56:58 -0500

    Add shirts component.

---
+++ src/shirts.tsx
@@ -0,0 +1,55 @@
+import { createSignal, createResource, Show } from "solid-js";
+
+const partsFileName = "/sprites/whole-shirt.svg"
+
+
+async function parseIt(fn) {
+  console.log("PARSEIT")
+  const res = await fetch(fn)
+  const txt = await res.text()
+  const parser = new DOMParser()
+  const svgDoc = parser.parseFromString(txt, "image/svg+xml")
+  const symbols = svgDoc.querySelectorAll('symbol')
+
+  const z = Array.from(symbols).map((x) => x.getAttribute("id"))
+  
+  
+  console.log("Z", z)
+
+  const y = z.map((x) => `${fn}#${x}`)
+  console.log("Y", y)
+
+  return y
+}
+
+
+export default function Shirts () {
+
+  const [name1, setName1] = createSignal(0)
+  const [shirts, {refetch}] = createResource(partsFileName, parseIt)
+
+  refetch()
+
+  function name2() {
+    return name1() + 1
+  }
+
+  function clicky() {
+    if (4 === name1()) {
+      setName1(0)
+    } else {
+      setName1(name1() + 2)
+    }
+  }
+
+  return (
+    <div>
+    <Show when={!shirts.loading}>
+      <svg onClick={clicky}> 
+        <use href={shirts()[name1()]}></use>
+        <use href={shirts()[name2()]}></use>
+      </svg>
+    </Show>
+    </div>
+  )
+}
