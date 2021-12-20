c90ed2bdb76bdfd7480d170873bd2b479ad62974
Robin Millette <robin@millette.info>
2021-12-20 13:16:33 -0500

    Add (avatar) body component.

---
+++ src/components/body.tsx
@@ -0,0 +1,6 @@
+// import { createSignal, createResource, Show } from "solid-js"
+import AvatarItemV3 from "./avatar-item-v3"
+
+export default function Body() {
+  return <AvatarItemV3 partsFileName="/sprites/whole-body.svg" />
+}
