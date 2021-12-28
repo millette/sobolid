c23452e59a80b922fc8621b53893ea6187258c78
Robin Millette <robin@millette.info>
2021-12-28 13:47:42 -0500

    Fix script error (undefined var).

--- fix-vb.js
+++ fix-vb.js
@@ -7,6 +7,6 @@ import fg from "fast-glob"
 fg.sync("public/sprites/whole-*.svg").forEach((z) => {
   const x = readFileSync(z, "utf-8")
   const y = x.replace(/ viewBox="[-0-9 ]+"/g, ' viewBox="0 0 560 560"')
-  console.log(p.length, z, x.length, y.length, x.length - y.length)
+  console.log(z, x.length, y.length, x.length - y.length)
   writeFileSync(`v2/${z}`, y)
 })
