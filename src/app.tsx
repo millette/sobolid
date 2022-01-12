3c17a32724cc3325b167c3aa8c34bc7dacc4e32d
Robin Millette <robin@millette.info>
2022-01-12 16:27:24 -0500

    Add path prefix for github pages (subdir) deploy.

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import { NavLink, useRoutes, useLocation } from "solid-app-router"
 import { MetaProvider, Title } from "solid-meta"
 
 // self
-import { routes } from "./routes"
+import { pathPrefix, routes } from "./routes"
 import "./styles.css"
 
 function App(): JSX.Element {
@@ -17,22 +17,35 @@ function App(): JSX.Element {
       <nav class="bg-gray-200 text-gray-900 px-4">
         <ul class="flex items-center">
           <li class="py-2 px-4">
-            <NavLink end href="/" class="no-underline hover:underline">
+            <NavLink
+              end
+              href={`${pathPrefix}/`}
+              class="no-underline hover:underline"
+            >
               Home
             </NavLink>
           </li>
           <li class="py-2 px-4">
-            <NavLink href="/about" class="no-underline hover:underline">
+            <NavLink
+              href={`${pathPrefix}/about`}
+              class="no-underline hover:underline"
+            >
               About
             </NavLink>
           </li>
           <li class="py-2 px-4">
-            <NavLink href="/credits" class="no-underline hover:underline">
+            <NavLink
+              href={`${pathPrefix}/credits`}
+              class="no-underline hover:underline"
+            >
               Credits
             </NavLink>
           </li>
           <li class="py-2 px-4">
-            <NavLink href="/error" class="no-underline hover:underline">
+            <NavLink
+              href={`${pathPrefix}/error`}
+              class="no-underline hover:underline"
+            >
               Error
             </NavLink>
           </li>
