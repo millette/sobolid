c678518f94b83dcb34cade92016cc97f27eb3a22
Robin Millette <robin@millette.info>
2022-01-13 15:10:42 -0500

    Updates

--- assets/404.d95e6cb9.js
+++
@@ -1,1 +0,0 @@
-import{t}from"./vendor.bae5e718.js";const o=t(`<section class="text-gray-700 p-8"><h1 class="text-2xl font-bold">404: Not Found</h1><p class="mt-4">It's gone \u{1F61E}</p></section>`);function s(){return o.cloneNode(!0)}export{s as default};
