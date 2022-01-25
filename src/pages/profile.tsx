732051452ee6aaeea77387c860e40a03320a102f
Robin Millette <robin@millette.info>
2022-01-24 21:39:21 -0500

    Fix profile page.

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -7,7 +7,7 @@ import { Show } from "solid-js"
 function Profile() {
   return (
     <div>
-      <Show when={session} fallback="Must login">
+      <Show when={session.user} fallback="Must login">
         <h1 class="text-2xl font-bold">Profile</h1>
         <pre>{JSON.stringify(session, null, 2)}</pre>
       </Show>
