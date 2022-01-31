af9d90b134b1b159317f7d0433123a8b84eaf00d
Robin Millette <robin@millette.info>
2022-01-31 00:33:46 -0500

    Cleanup github profile component.

--- src/components/github-profile.tsx
+++ src/components/github-profile.tsx
@@ -5,11 +5,6 @@ import { Dl, DtDd } from "~/components/dl"
 // npm
 import { Show } from "solid-js"
 
-const hashedVersion =
-  import.meta.env.MODE === "production"
-    ? `v.${import.meta.env.VITE_HASHED_VERSION}`
-    : import.meta.env.MODE
-
 function providerIdentity({ identities }, p) {
   return identities.find(({ provider }) => provider === p)?.identity_data
 }
@@ -34,31 +29,3 @@ function GithubProfile() {
 }
 
 export default GithubProfile
-
-/*
-const i = [
-  {
-    "id": "50741",
-    "user_id": "0031e817-32a7-48ad-a0bc-c26096e48923",
-    "identity_data": {
-      "avatar_url": "https://avatars.githubusercontent.com/u/50741?v=4",
-      "email": "robin@millette.info",
-      "email_verified": true,
-      "full_name": "Robin Millette",
-      "iss": "https://api.github.com",
-      "name": "Robin Millette",
-      "preferred_username": "millette",
-      "provider_id": "50741",
-      "sub": "50741",
-      "user_name": "millette"
-    },
-    "provider": "github",
-    "last_sign_in_at": "2022-01-18T15:08:19.162122Z",
-    "created_at": "2022-01-18T15:08:19.16216Z",
-    "updated_at": "2022-01-18T15:08:19.16216Z"
-  }
-]
-
-const x = providerIdentity({ identities: i }, "github")
-console.log(x)
-*/
