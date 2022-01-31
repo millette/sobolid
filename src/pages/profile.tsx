747cec890e3024441538b167455a781522c06164
Robin Millette <robin@millette.info>
2022-01-31 00:32:56 -0500

    Improve user profile page.

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,17 +1,35 @@
 // self
 import { session } from "~/utils/session"
+import GithubProfile from "~/components/github-profile"
+import ago from "~/utils/ago"
+import { Dl, DtDd } from "~/components/dl"
 
 // npm
 import { Show } from "solid-js"
+import { Title } from "solid-meta"
+
+const hashedVersion =
+  import.meta.env.MODE === "production"
+    ? `v.${import.meta.env.VITE_HASHED_VERSION}`
+    : import.meta.env.MODE
+
+//       <pre>{JSON.stringify(session, null, 2)}</pre>
 
 function Profile() {
   return (
-    <div>
-      <Show when={session.user} fallback="Must login">
-        <h1 class="text-2xl font-bold">Profile</h1>
-        <pre>{JSON.stringify(session, null, 2)}</pre>
-      </Show>
-    </div>
+    <Show when={session.user} fallback="Must login">
+      <Title>
+        {session.user.email} profile ({hashedVersion})
+      </Title>
+      <h1 class="text-2xl font-bold">{session.user.email} profile</h1>
+
+      <Dl>
+        <DtDd dt="Membership" dd={ago(session.user.created_at)} />
+        <DtDd dt="Logged in" dd={ago(session.user.last_sign_in_at)} />
+      </Dl>
+
+      <GithubProfile />
+    </Show>
   )
 }
 
